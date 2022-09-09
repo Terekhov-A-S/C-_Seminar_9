466bcec58f6308a7c7ae7ee0e1e815430e928e84
KoT <Kotto@student.su>
2022-09-09 18:50:48 +0400

    added task_66

---
+++ Task_66/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Clear();
+Console.WriteLine("Введите число, обозначающее начало промежутка чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число, обозначающее конец промежутка чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int count = m; count <= n; count++) { sum = sum + count; }
+Console.Write($"{sum}");
+Console.WriteLine();
