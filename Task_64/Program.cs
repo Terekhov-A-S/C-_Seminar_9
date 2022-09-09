1223ff5d8ba1730829df954792c25327db2f88d4
KoT <Kotto@student.su>
2022-09-09 18:50:20 +0400

    added task_64

---
+++ Task_64/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+Console.Clear();
+Console.WriteLine("Введите число, обозначающее начало промежутка чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число, обозначающее конец промежутка чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int count = m; count < n; count++) { Console.Write($"{count} "); }
+Console.WriteLine();
